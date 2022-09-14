f1cdc34d2d72664f4ada9b2ac73f1d44943a6553
qzydustin <44362429+qzydustin@users.noreply.github.com>
2022-09-14 01:07:14 -0700

    Initial Commit

---
+++ process_syntax_tree.c
@@ -0,0 +1,27 @@
+/*
+ * File: process_syntax_tree.c
+ * Purpose: Code to traverse the syntax tree of each function and
+ *     perform any desired actions.
+ */
+
+#include <stdio.h>
+#include "syntax-tree.h"
+
+extern void printSyntaxTree(tnode *t, int n, int depth);
+
+/*
+ * process_syntax_tree(fn_name, fn_body) -- fn_name is a poiner to
+ * the symbol table entry of the function being processed (which can
+ * be used, for example, to get the name of the fucntion); fn_body 
+ * is a pointer to the root of the syntax tree of the function body.
+ * This function can be used to carry out tree traversals of the
+ * function's syntax tree.
+ */
+void process_syntax_tree(symtabnode *fn_name, tnode *fn_body) {
+#ifdef DEBUG
+  printf("@@FUN: %s\n", fn_name->name);
+  printf("@@BODY:\n");
+  printSyntaxTree(fn_body, 4, 0);
+  printf("-----\n");
+#endif
+}
