bbfe88b299ac654f664b5cd95e35e09b0efba3f4
Zhenyu Qi <qzydustin@hotmail.com>
2024-02-12 16:03:22 -0700

    Update Task form

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,8 +1,7 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
-
-  class Meta:
-    model = Task
-    fields = '__all__'
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'content', 'priority', 'status', 'due_date', 'owner']
