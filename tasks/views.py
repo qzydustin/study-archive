5a67026881725fed514b47ab1a11deecc27d4fa0
Zhenyu Qi <qzydustin@hotmail.com>
2024-02-12 17:44:21 -0700

    Add login_required to tasks, auto fill the owner

--- tasks/views.py
+++ tasks/views.py
@@ -1,20 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
 
+@login_required
 def create_task(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('list_tasks')
+            task = form.save(commit=False)
+            task.owner = request.user
+            task.save()
+            return redirect("list_tasks")
     else:
         form = TaskForm()
-    return render(request, 'create_task.html', {'form': form})
+    return render(request, "create_task.html", {"form": form})
 
 
+@login_required
 def list_tasks(request):
-    # Retrieve all tasks from the database
-    tasks = Task.objects.all()
-    return render(request, 'list_tasks.html', {'tasks': tasks})
+    # Display only tasks owned by the logged-in user
+    tasks = Task.objects.filter(owner=request.user)
+    return render(request, "list_tasks.html", {"tasks": tasks})
